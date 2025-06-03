d532275f875e353c44b02fd1293deced4041703d
Danyelke <118920510+Danyelke@users.noreply.github.com>
2025-06-03 17:35:22 +0200

    the game window can be closed now

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while 1 == 0:
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
         screen.fill(000)
         pygame.display.flip()
 
